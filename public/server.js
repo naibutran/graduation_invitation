0405cdb8cdc9a955f9970204218bf1494c7d2e3d
naibutran <anhtran.nb117@gmail.com>
2024-11-21 11:57:42 +0700

    move to public

--- public/server.js
+++ public/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 
 // Danh sách ghế ngồi
 let seatMap = Array(10).fill(null).map(() => Array(10).fill(null));
