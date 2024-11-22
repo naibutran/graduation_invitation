0335437ff8253eccbc782c28d22cdeaeaa3b2ee8
naibutran <anhtran.nb117@gmail.com>
2024-11-22 14:53:02 +0700

    fix dir 2

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { Pool } = require('pg');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 6969;
