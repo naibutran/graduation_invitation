b758bc233d1b0ec9fd99dd2c870e4738ccad4cca
Trần Hoàng Anh <anhtran.nb117@gmail.com>
2024-11-25 04:48:03 +0700

    Update server.js

--- server.js
+++ server.js
@@ -125,7 +125,8 @@ function delete_name(inp){
   (async () => {
     try {
       console.log('Deleting');
-      const res = await pool.query("DELETE FROM seats WHERE name = $1", [inp], (error, res));
+      const res = await pool.query("DELETE FROM seats WHERE name = $1", inp);
+  
       // console.log('All data in the "seats" table has been truncated and ID counter reset.');
       
     } catch (err) {
